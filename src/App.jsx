6616ee4436d13751c575fceff4211ca43f6b5f84
Ryan Baumgardner <ryan.baumgardner@protonmail.com>
2022-09-30 13:16:42 -0400

    refactor: refactoring to make things easier

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,213 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { createEffect } from "solid-js";
+import styles from "./App.module.css";
 import data from "./data.json";
+import scoreData from "./data-scores.json";
 
-const cleanedData = data.reduce(( acc, curr ) => {
-	const bookmaker = curr.bookmakers.filter( bookmaker => {
-		if ( bookmaker.key === "barstool" ) {
-			return bookmaker;
+const combinedData = data.map( (game) => {
+	const gameScore = scoreData.find( g => g.id === game.id );
+	const homeTeam = game.home_team;
+	const awayTeam = game.away_team;
+	let homeTeamSpread, awayTeamSpread;
+	game.bookmakers.map( book => {
+		if ( book.key === "barstool" ) {
+			const teams = book.markets[ 0 ].outcomes;
+			homeTeamSpread = teams.find( team => team.name === homeTeam ).point;
+			awayTeamSpread = teams.find( team => team.name === awayTeam ).point;
+			console.log( awayTeamSpread, homeTeamSpread );
 		}
-
-		return undefined;
 	} );
 
-	curr.bookmakers = bookmaker;
+	if ( gameScore.completed ) {
+		const homeTeamScore = gameScore.scores.find( score => score.name === homeTeam );
+		const awayTeamScore = gameScore.scores.find( score => score.name === awayTeam );
+		return {
+			id: game.id,
+			completed: gameScore.completed,
+			commence_time: gameScore.commence_time,
+			home: {
+				team: homeTeam,
+				score: homeTeamScore.score,
+				spread: homeTeamSpread,
+			},
+			away: {
+				team: awayTeam,
+				score: awayTeamScore.score,
+				spread: awayTeamSpread,
+			}
+		}
+	} else {
+		return {
+			id: game.id,
+			completed: gameScore.completed,
+			commence_time: gameScore.commence_time,
+			home: {
+				team: homeTeam,
+				score: null,
+				spread: homeTeamSpread,
+			},
+			away: {
+				team: awayTeam,
+				score: null,
+				spread: awayTeamSpread,
+			}
+		}
+	}
+} );
 
-	acc.push( curr );
+console.log( combinedData );
 
-	return acc;
-}, [])
-
-const date = new Date();
+/* [ */
+/* 	{ */
+/* 		game_id: "123", */
+/* 		selected_team: "blah", */
+/* 		spread: 0 */
+/* 	} */
+/* ] */
 
 function App() {
-  	const handleChecked = (e, id) => {
-		const game = document.getElementById(id);
-		const firstCheckbox = game.querySelectorAll( "input" )[ 0 ];
-		const secondCheckbox = game.querySelectorAll( "input" )[ 1 ];
+	createEffect(() => {
+		const currentData = window.localStorage.getItem("week");
 
-		if ( e.target.checked ) {
-			console.log( e.target.labels[ 0 ].innerText );
-			window.localStorage.setItem( "week", e.target.labels[ 0 ].innerText );
+		if (currentData) {
+			const parsedData = JSON.parse(currentData);
+			Object.keys(parsedData).map((id) => {
+				const game = document.getElementById(id);
+				const team = parsedData[id].name;
+				const firstLabel = game.querySelectorAll("label")[0];
+				const firstCheckbox = game.querySelectorAll("input")[0];
+				const secondCheckbox = game.querySelectorAll("input")[1];
+				const team1 = firstLabel.innerText.includes(team);
+
+				if (team1) {
+					firstCheckbox.checked = true;
+					secondCheckbox.disabled = true;
+				} else {
+					secondCheckbox.checked = true;
+					firstCheckbox.disabled = true;
+				}
+			});
+		}
+	});
+
+	createEffect(() => {
+		const currentData = window.localStorage.getItem( "week" );
+
+		if ( currentData ) {
+			const parsedData = JSON.parse( currentData );
+
+			const scores = scoreData.reduce((acc, curr) => {
+				const foundGame = parsedData[ curr.id ];
+				if ( foundGame && curr.completed ) {
+					const winner = curr.scores[ 0 ].score > curr.scores[ 1 ].score ? {
+							team: curr.scores[ 0 ].name,
+							amount: curr.scores[ 0 ].score - curr.scores[ 1 ].score
+						} : {
+							team: curr.scores[ 1 ].name,
+							amount: curr.scores[ 1 ].score - curr.scores[ 0 ].score
+						}
+
+					if ( winner.team === foundGame.name ) {
+						const diff = winner.amount + foundGame.point;
+
+						diff >= 0 ? acc.jon++ : acc.chuck++;
+
+						console.log( { curr, foundGame, winner } );
+					} else {
+						const diff = winner.amount - foundGame.point;
+
+						diff >= 0 ? acc.chuck++ : acc.jon++;
+
+						console.log( { curr, foundGame, winner } );
+					}
+
+				}
+
+				return acc;
+
+			}, { jon: 0, chuck: 0 } )
+
+			console.log( scores );
+		}
+	});
+
+	const handleChecked = (e, id, name, point) => {
+		const game = document.getElementById(id);
+		const firstCheckbox = game.querySelectorAll("input")[0];
+		const secondCheckbox = game.querySelectorAll("input")[1];
+
+		if (e.target.checked) {
+			const currentData = window.localStorage.getItem("week");
+
+			if (currentData) {
+				const parsedData = JSON.parse(currentData);
+				const data = {
+					...parsedData,
+					[id]: {
+						name,
+						point,
+					},
+				};
+				window.localStorage.setItem("week", JSON.stringify(data));
+			} else {
+				window.localStorage.setItem(
+					"week",
+					JSON.stringify({ [id]: { name, point } })
+				);
+			}
 		}
 
-		if ( firstCheckbox.checked ) {
+		if (firstCheckbox.checked) {
 			secondCheckbox.disabled = true;
 		} else {
 			secondCheckbox.disabled = false;
 		}
 
-		if ( secondCheckbox.checked ) {
+		if (secondCheckbox.checked) {
 			firstCheckbox.disabled = true;
 		} else {
 			firstCheckbox.disabled = false;
 		}
 	};
 
-	return cleanedData.map( game => {
-		const day = date.getDate( game.commence_time );
-		const year = date.getFullYear( game.commence_time );
-		const timestamp = day.toString() + year.toString();
-		console.log( { day, year, timestamp } );
-		const spread = game.bookmakers[ 0 ]?.markets[ 0 ].outcomes;
+	return combinedData.map((game) => {
+		const home = game.home;
+		const away = game.away;
 
-		if ( spread ) {
-			const team1 = spread[ 0 ];
-			const team2 = spread[ 1 ];
-
-			return (
-				<div key={ game.id }>
-					<li id={ game.id }>
-						<label>
-							<input type="checkbox" onChange={ e => handleChecked(e, game.id ) } />
-							{ team1.name } { team1.point }
-						</label>
-						<label>
-							<input type="checkbox" onChange={ e => handleChecked(e, game.id ) } />
-							{ team2.name } { team2.point }
-						</label>
-					</li>
-				</div>
-			)
-		}
-
-		return null;
-	} );
-
+		return (
+			<div key={game.id}>
+				<li id={game.id}>
+					<label>
+						<input
+							type="checkbox"
+							onChange={(e) =>
+								handleChecked(
+									e,
+									game.id,
+									home.team,
+									home.spread
+								)
+							}
+						/>
+						Home: { home.team } {home.spread} { home.score ?? "" }
+					</label>
+					<label>
+						<input
+							type="checkbox"
+							onChange={(e) =>
+								handleChecked(
+									e,
+									game.id,
+									away.team,
+									away.spead
+								)
+							}
+						/>
+						Away: {away.team} {away.spread} { away.score ?? "" }
+					</label>
+				</li>
+			</div>
+		)
+	} )
 }
 
 export default App;
